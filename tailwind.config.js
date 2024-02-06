d32245821e6e2ddb397d8a58188c38f1d2366606
Na3ar-17 <gavruk2017@gmail.com>
2024-02-06 11:38:38 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      text: '#fff',
+      background: '#092635',
+      'background-2': '#9EC8B9',
+      'dark-green': '#1B4242',
+      sage: '#5C8374',
+    },
+  },
+  plugins: [],
+}
